24c7c5ba3ec7cbd641ba5e355be9367dcbf58fb4
pompaFunebris <jakub.dobosz01@gmail.com>
2024-05-01 18:27:25 +0200

    wip

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-views = Blueprint("home", __name__, static_folder="static", template_folder="templates")
+views = Blueprint("views_blueprint", __name__, static_folder="static", template_folder="templates")
 
 
 @views.route("/home")
@@ -12,3 +12,26 @@ def render_home():
 @views.route('/kontakt/')
 def contact():
     return render_template("contact.html")
+
+
+@views.route('/about/')
+def about():
+    return render_template("about.html")
+
+
+@views.route('/wiedza')
+def carbon_footprint_article():
+    section = request.args.get('section')
+    if section == 'slad-weglowy':
+        return render_template('carbon-footprint-article.html', section='slad-weglowy')
+    elif section == 'po-co-liczyc-slad-weglowy':
+        return render_template('carbon-footprint-article.html', section='po-co-liczyc-slad-weglowy')
+    elif section == 'jak-liczyc-slad-weglowy':
+        return render_template('carbon-footprint-article.html', section='jak-liczyc-slad-weglowy')
+    else:
+        return render_template('carbon-footprint-article.html')  # Default render
+
+
+@views.route('/kalkulator-emisji/')
+def emission_calculator():
+    return render_template("emission-calculator.html")
