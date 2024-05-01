e0d56cdf2f1f814f2da9f6066722dc2b7a7104ff
pompaFunebris <jakub.dobosz01@gmail.com>
2024-05-01 13:34:38 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)  # Flask constructor
+
+
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/')
+def hello():
+    return 'HELLO'
+
+
+if __name__ == '__main__':
+    app.run()
+
