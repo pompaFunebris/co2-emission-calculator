c7c75a21b6daea3e679e210c3a3c68e74c10812f
pompaFunebris <jakub.dobosz01@gmail.com>
2024-05-04 21:19:48 +0200

    wip

--- website/views.py
+++ website/views.py
@@ -24,31 +24,38 @@ def about():
 @views.route('/kalkulator-emisji/', methods=['GET', 'POST'])
 def emission_calculator():
     if request.method == 'POST':
-        material_names = request.form.getlist('material_name[]')
-        material_volumes = request.form.getlist('material_volume[]')
+        total_emission = 0
+        chosen_materials = []
 
-        insulator_names = request.form.getlist('insulator_name[]')
-        insulator_volumes = request.form.getlist('insulator_volume[]')
+        # Define the categories and corresponding CSV files
+        categories = ['material', 'insulator', 'finishing', 'other']
+        kategorie = ['Materiał główny', 'Termoizolacje', 'Wykończenia', 'Pozostałe']
+        files = ['main-materials.csv', 'insulators.csv', 'finishing-materials.csv', 'other-materials.csv']
 
-        finishing_names = request.form.getlist('finishing_name[]')
-        finishing_volumes = request.form.getlist('finishing_volume[]')
+        for category, file, kategoria in zip(categories, files, kategorie):
+            names = request.form.getlist(f'{category}_name[]')
+            volumes = request.form.getlist(f'{category}_volume[]')
+            material_data = []
 
-        other_names = request.form.getlist('other_name[]')
-        other_quantities = request.form.getlist('other_volume[]')
+            with open(f'website/static/materials/{file}', newline='', encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    label = row['Material']
+                    value = row['Value']
+                    unit = row['Unit']
 
-        print(f"Mat names: {material_names}")
-        print(f"Mat volumes: {material_volumes}")
+                    if label in names:
+                        index = names.index(label)
+                        volume = volumes[index]
+                        total_emission += float(volume) * float(value)
+                        material_data.append({'name': label, 'volume': volume, 'unit': unit})
 
-        print(f"insulator_names: {insulator_names}")
-        print(f"insulator_volumes: {insulator_volumes}")
+            chosen_materials.append({'category': kategoria, 'data': material_data})
 
-        print(f"finishing_names: {finishing_names}")
-        print(f"finishing_volumes: {finishing_volumes}")
+        # Convert total emission to metric tons
+        total_emission = round(total_emission / 1000, 2)
 
-        print(f"other_names: {other_names}")
-        print(f"other_quantities: {other_quantities}")
-
-        return 'Data received successfully!'
+        return render_template('emission-calculator.html', total_emission=total_emission, chosen_materials=chosen_materials)
     else:
         return render_template('emission-calculator.html')
 
